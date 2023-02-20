59fd1b9915d521c9201811427bd4c3ed5bf62739
Chai Kanchan <cdkanchan@gmail.com>
2023-02-20 18:51:29 +0000

    Updated to swift tools 5.6

--- Package.swift
+++ Package.swift
@@ -1,10 +1,13 @@
-// swift-tools-version:4.0
+// swift-tools-version:5.6
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "cdlztosqlite",
+    products: [
+        .executable(name: "cdlztosqlite", targets: ["cdlztosqlite"])
+    ],
     dependencies: [
         .package(url: "https://github.com/stephencelis/SQLite.swift.git", from: "0.11.5"),
         .package(url: "https://github.com/ckanchan/CDKSwiftOracc", from: "0.5.6"),
@@ -14,9 +17,14 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
+        .executableTarget(
             name: "cdlztosqlite",
-            dependencies: ["CDKSwiftOracc", "SQLite", "ZIPFoundation"]),
+            dependencies: [
+                .product(name: "CDKSwiftOracc", package: "cdkswiftoracc"),
+                .product(name:"SQLite", package:"SQLite.swift"),
+                .product(name:"ZIPFoundation", package: "zipfoundation")
+            ]
+        )
     ]
 )
 
